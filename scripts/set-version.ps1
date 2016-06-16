e869741c0edbef7d89bfd95e2bcf46da60787570
Rob Prouse <rob@prouse.org>
2016-06-16 11:37:12 -0400

    Clearly I am dislexic

--- scripts/set-version.ps1
+++ scripts/set-version.ps1
@@ -13,7 +13,7 @@ $PSScriptFilePath = (Get-Item $MyInvocation.MyCommand.Path).FullName
 $ScriptDir = Split-Path -Path $PSScriptFilePath -Parent
 $SolutionRoot = Split-Path -Path $ScriptDir -Parent
 
- $ProjectJsonPath = Join-Path -Path $SolutionRoot -ChildPath "src\NUnit.Agent.Portable\project.json"
+ $ProjectJsonPath = Join-Path -Path $SolutionRoot -ChildPath "src\NUnit.Portable.Agent\project.json"
  (gc -Path $ProjectJsonPath) `
  	-replace "(?<=`"version`":\s`")[.\w-]*(?=`",)", "$ReleaseVersionNumber$PreReleaseName" |
  	sc -Path $ProjectJsonPath -Encoding UTF8
